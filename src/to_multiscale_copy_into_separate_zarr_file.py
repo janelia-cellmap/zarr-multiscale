7f4c50cafaa58943ad4efdfd74925f2da0f10dcb
Zubov <zubov452@gmail.com>
2024-07-17 10:58:51 -0400

    Added cli

--- src/to_multiscale_copy_into_separate_zarr_file.py
+++ src/to_multiscale_copy_into_separate_zarr_file.py
@@ -15,8 +15,16 @@ from dask.distributed import Client, wait
 from numcodecs import Zstd
 from toolz import partition_all
 from dask_jobqueue import LSFCluster
-import click
 
+
+src_store = zarr.NestedDirectoryStore('path_to_parent_array_group')
+src_root = zarr.open_group(store=src_store, mode = 'r')
+src_arr = src_root['name_of_the_array']
+out_chunks = (212,) * 3
+
+
+dest_store = zarr.NestedDirectoryStore('path_to_array_group')
+dest_root = zarr.open_group(store=dest_store, mode='a')
 def upscale_slice(slc: slice, factor: int):
     return slice(slc.start * factor, slc.stop * factor, slc.step)
 
@@ -34,41 +42,34 @@ def downsample_save_chunk_mode(
         dest[out_slices] = ds_data
     return 1
 
-def create_multiscale(z_root: zarr.Group, client: Client, num_workers: int, data_origin: str):
+def create_multiscale(src_arr: zarr.Array, dest_root: zarr.Group, out_chunks: Tuple[int, ...], client: Client, num_workers: int, comp: Codec):
     # store original array in a new .zarr file as an arr_name scale
-    z_attrs = z_root.attrs.asdict() 
-    print(z_attrs)
-    base_scale = z_attrs['multiscales'][0]['datasets'][0]['coordinateTransformations'][0]['scale']
-    base_trans = z_attrs['multiscales'][0]['datasets'][0]['coordinateTransformations'][1]['translation']
-    num_levels = 8
+    #z_attrs = z_root.attrs.asdict() 
+    #print(z_attrs)
+    # base_scale = z_attrs['multiscales'][0]['datasets'][0]['coordinateTransformations'][0]['scale']
+    # base_trans = z_attrs['multiscales'][0]['datasets'][0]['coordinateTransformations'][1]['translation']
+    num_levels = 7
     client.cluster.scale(num_workers)
-    
-    level = 1
-    source_shape = z_root[f's{level-1}'].shape
-    while all([dim > 32 for dim in source_shape]):
-    #for level in range(1, num_levels - 1):
-        print(source_shape)
+    source_arr = src_arr
+    for level in range(1, num_levels - 1):
         print(f'{level=}')
-        source_arr = z_root[f's{level-1}']
+        #source_arr = z_root[f's{level-1}']
         source_darr = da.from_array(source_arr, chunks=(-1,-1,-1))
-        if data_origin == 'labels':
-            res_level_template = multiscale(source_darr, windowed_mode, 2)[1].data
-        elif data_origin == 'raw':
-            res_level_template = multiscale(source_darr, windowed_mean, 2)[1].data
+        res_level_template = multiscale(source_darr, windowed_mode, 2)[1].data
 
         start_time = time.time()
-        dest_arr = z_root.require_dataset(
+        dest_arr = dest_root.require_dataset(
             f's{level}', 
             shape=res_level_template.shape, 
-            chunks=source_arr.chunks, 
+            chunks=out_chunks, 
             dtype=res_level_template.dtype, 
-            compressor=source_arr.compressor, 
+            compressor=comp, 
             dimension_separator='/',
             fill_value=0,
             exact=True)
-        
-        assert dest_arr.chunks == source_arr.chunks
-        out_slices = slices_from_chunks(normalize_chunks(source_arr.chunks, shape=dest_arr.shape))
+        print(dest_arr.chunks)
+        assert dest_arr.chunks == out_chunks
+        out_slices = slices_from_chunks(normalize_chunks(out_chunks, shape=dest_arr.shape))
         #break the slices up into batches, to make things easier for the dask scheduler
         out_slices_partitioned = tuple(partition_all(100000, out_slices))
         for idx, part in enumerate(out_slices_partitioned):
@@ -79,27 +80,36 @@ def create_multiscale(z_root: zarr.Group, client: Client, num_workers: int, data
             # wait for all the futures to complete
             result = wait(fut)
             print(f'Completed {len(part)} tasks in {time.time() - start}s')
-        sn = [dim * pow(2, level) for dim in base_scale]
-        trn = [(dim * (pow(2, level - 1) -0.5))+tr for (dim, tr) in zip(base_scale, base_trans)]
+        # sn = [dim * pow(2, level) for dim in base_scale]
+        # trn = [(dim * (pow(2, level - 1) -0.5))+tr for (dim, tr) in zip(base_scale, base_trans)]
 
-        z_attrs['multiscales'][0]['datasets'].append({'coordinateTransformations': [{"type": "scale",
-                    "scale": sn}, {"type" : "translation", "translation" : trn}],
-                    'path': f's{level}'})
+        # z_attrs['multiscales'][0]['datasets'].append({'coordinateTransformations': [{"type": "scale",
+        #             "scale": sn}, {"type" : "translation", "translation" : trn}],
+        #             'path': f's{level}'})
         
-        level += 1
-        source_shape = res_level_template.shape
+        source_arr = dest_root[f's{level}']
     
-    z_root.attrs['multiscales'] = z_attrs['multiscales']
-        
-@click.command()
-@click.option('--src','-s',type=click.Path(exists = True),help='Input .zarr file location.')
-@click.option('--workers','-w',default =100,type=int,help = "Number of dask workers")
-@click.option('--data_origin','-do',type=str,help='Different data requires different type of interpolation. Raw fibsem data - use \'raw\', for segmentations - use \'segmentation\'')
-def cli(src, workers, data_origin):
+    #z_root.attrs['multiscales'] = z_attrs['multiscales']
     
-    src_store = zarr.NestedDirectoryStore(src)
-    src_root = zarr.open_group(store=src_store, mode = 'a')
+
+# def add_multiscale_metadata(dest_root):
+#     #populate .zattrs
+#     path = os.path.join(os.path.abspath(os.sep), os.getcwd(), 'src/zarr_attrs_template.json' )
+#     print(path)
+#     f_zattrs_template = open(path )
+#     z_attrs = json.load(f_zattrs_template)
     
+#     #z_attrs['multiscales'][0]['axes'] = [{"name": axis, 
+#     #                                    "type": "space",
+#     #                                    "unit": "nanometer"} for axis in ['x', 'y', 'z']]
+                                                                                
+#     z_attrs['multiscales'][0]['version'] = '0.4'
+#     z_attrs['multiscales'][0]['name'] = dest_root.name
+#     return z_attrs
+
+if __name__ == '__main__':
+    # store_multiscale = cw.cluster_compute("local")(create_multiscale)
+    # store_multiscale(src_root,(64, 64, 64),  Zstd(level=6))
     num_cores = 1
     cluster = LSFCluster(
         cores=num_cores,
@@ -115,8 +125,6 @@ def cli(src, workers, data_origin):
         text_file.write(str(client.dashboard_link))
     print(client.dashboard_link)
 
-    create_multiscale(z_root=src_root, client=client, num_workers=workers, data_origin=data_origin)
-    
-if __name__ == '__main__':
-    cli()
+    create_multiscale(src_arr=src_arr, dest_root=dest_root, out_chunks=out_chunks, client=client, num_workers=1200, comp=Zstd(level=6))
+ 
 
